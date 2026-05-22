4ff11494a8f4bba4afe6555ddd79331ac5420640
Peng Cao <caopengau@gmail.com>
2026-05-23 00:21:51 +1000

    chore: upgrade cloudflare provider to 6.15.0 in sst.config

--- sst.config.ts
+++ sst.config.ts
@@ -30,7 +30,7 @@ export default $config({
           region,
           version: '7.23.0',
         },
-        ...(input?.stage !== 'local' ? { cloudflare: '6.13.0' } : {}),
+        ...(input?.stage !== 'local' ? { cloudflare: '6.15.0' } : {}),
       },
       defaults: {
         function: {
