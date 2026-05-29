409383cc4d05d8751af08c26ac0a3318d0faa500
Peng Cao <caopengau@gmail.com>
2026-05-30 03:25:59 +1000

    chore: remove stale bridge file

--- apps/dashboard/src/extensions/bridge.tsx
+++
@@ -1,4 +0,0 @@
-/** Generated Bridge */
-export * from '/Users/pengcao/projects/goldex/apps/goldex-dashboard/index.tsx';
-import ext from '/Users/pengcao/projects/goldex/apps/goldex-dashboard/index.tsx';
-export default ext;
